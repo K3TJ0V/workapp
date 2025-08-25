fcc32d2156ae7b4fe7b3f005b6cffa2936bc3aa6
Wojciech Górka <wojowany@gmail.com>
2025-08-25 19:46:25 +0200

    first creator models, workapp/settings - db settings setup, workapp/urls - main urls setup

--- backend/creator/models.py
+++ backend/creator/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=800, null=True)
+    video = models.URLField(max_length=255, null=True)
+    comment = models.CharField(max_length=800, null=True)
+
+
+class Workout(models.Model):
+    descriptive_name = models.CharField(max_length=255, null=True)
+
+
+class Set(models.Model):
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField(null=True)
+    time = models.PositiveIntegerField(null=True)
+    tempo = models.CharField(max_length=4, null=True)
+    rir = models.CharField(max_length=3, null=True)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
