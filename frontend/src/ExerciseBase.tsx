e264192fac8b255df0dbf283d51078f7e01c7a0b
Wojciech Górka <wojowany@gmail.com>
2025-09-06 20:27:37 +0200

    small changes in Exercise class - set some properties as optional, styled ExerciseBase component, soon ExTiles will be a standalone component

--- frontend/src/ExerciseBase.tsx
+++ frontend/src/ExerciseBase.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './styles/ExerciseBase.scss'
 import useFetchGet from './utils/useFetchGet'
+import play from './assets/play.svg'
+import searchIcon from './assets/search.svg'
 import { Exercise } from './utils/classes'
 
 function ExerciseBase() {
@@ -13,18 +15,44 @@ function ExerciseBase() {
         const newEx = new Exercise(item.id, item.name, item.description, item.video, item.comment)
         result.push(newEx)
       })
+    }else if(Error){
+      console.log(Error);
     }
     setExercises(result)
   }, [data])
 
   return (
     <>
+      <button className='main__addEx'><p className='highlight'>+</p><br/>Add new exercise</button>
+      <label htmlFor="search" className='search-label'>
+        <input className='main__searchBar' type="text" name="search" id="search" placeholder='Eg. Bench press' />
+        <img src={searchIcon} alt="search icon" className='searchIcon'/>
+      </label>
       {isLoading && <p>Loading....</p>}
-      {Error && console.log(Error)}
       {data && 
       <section className='main__exList'>
         {exercises.map((item)=>{
-        return <article className='main__exList--exTile' key={item.id}>{item.id}, {item.name}</article>
+        return <article className='main__exList--exTile' key={item.id}>
+            <h3 className="main__exList--exName">--- {item.name} ---</h3>
+            {item.description ? 
+              <fieldset className='main__exList--exDesc'>
+                <legend>Description</legend>
+                {item.description}
+              </fieldset>
+            : <p className='no-content'>No description</p>  
+            }
+            {item.comment ? 
+            <fieldset className='main__exList--exComment'>
+              <legend>Comment</legend>
+                {item.comment}
+            </fieldset>
+            : <p className='no-content'>No description</p> 
+            }
+            {item.video ? 
+            <a href={item.video} target='blank' className='main__exList--exVideo'><img className='img' src={play} alt="play icon"/>  Video</a> 
+            : <p className='no-content'>No video</p>
+            }
+          </article>
         })}  
       </section>}
     </>
--- frontend/src/utils/classes.ts
+++ frontend/src/utils/classes.ts
@@ -1,10 +1,10 @@
 export class Exercise{
     id:number;
     name:string;
-    description:string;
-    video:string;
-    comment:string;
-    constructor(id:number, name:string, desc:string, video:string, comment:string){
+    description:string | undefined;
+    video:string | undefined;
+    comment:string | undefined;
+    constructor(id:number, name:string, desc?:string, video?:string, comment?:string){
         this.id = id;
         this.name = name;
         this.description = desc;
