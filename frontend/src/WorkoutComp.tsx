4826ab04237ade9f6c9a2e19f7e2915c8abd4226
Wojciech Górka <wojowany@gmail.com>
2025-09-30 19:41:15 +0200

    fetching workouts data, building WorkoutItems instances, rendering workout components

--- frontend/src/WorkoutComp.tsx
+++ frontend/src/WorkoutComp.tsx
@@ -1,12 +1,13 @@
+import type { WorkoutItem } from "./utils/classes";
+
 
 interface WorkoutCompProps{
     id:number;
-    descriptive_name: string | undefined
+    descriptive_name: string | undefined;
+    workout_items: WorkoutItem[]; 
 }
 
-function WorkoutComp( {id, descriptive_name} : WorkoutCompProps){
-
-
+function WorkoutComp( {id, descriptive_name, workout_items} : WorkoutCompProps){
     return (
         <article className="workouts__item">
             <h3 className="workouts__item--name">{descriptive_name}</h3>
--- frontend/src/Workouts.tsx
+++ frontend/src/Workouts.tsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles/Workouts.scss'
-import { Workout } from './utils/classes'
+import { Workout, WorkoutItem } from './utils/classes'
 import WorkoutComp from './WorkoutComp'
+import useFetchGet from './hooks/useFetchGet'
+
+interface WorkoutWithItems{
+    id: number;
+    descriptive_name: string | undefined;
+    workout_items: WorkoutItem[];
+}
+
 
 function Workouts(){
-    const [workouts, setWorkouts] = useState<Workout[]>([{id: 1, descriptive_name: "null"},{id: 1, descriptive_name: "null"}])
+    const {data, isLoading, Error} = useFetchGet('creator/workouts', '8000')
+    const [workouts, setWorkouts] = useState<WorkoutWithItems[]>()
 
+    useEffect(()=>{
+        if(data){
+            let test : WorkoutWithItems[] = []
+            data.forEach((work : WorkoutWithItems)=>{
+                let result: WorkoutWithItems = {
+                    id: work.id,
+                    descriptive_name: work.descriptive_name,
+                    workout_items: []
+                }
+                work.workout_items.forEach((item:WorkoutItem)=>{
+                    const newItem = new WorkoutItem(item.id, item.sets, item.exercise, item.workout)
+                    item.weight ? newItem.weight = item.weight : null;
+                    item.reps ? newItem.reps = item.reps : null;
+                    item.time ? newItem.time = item.time : null;
+                    item.tempo ? newItem.tempo = item.tempo : null;
+                    result.workout_items.push(newItem)
+                })
+                test.push(result)
+            })
+            setWorkouts(test)
+        }
+    }, [data])
     return(
         <>
             <section className="workouts">
+                {isLoading && "Loading..."}
+                {Error && "Error while downloading content"}
                 {workouts && workouts.map((item)=>{
-                    return <WorkoutComp id={item.id} descriptive_name={item.descriptive_name}/>
+                    return <WorkoutComp key={item.id} id={item.id} descriptive_name={item.descriptive_name} workout_items={item.workout_items}/>
                 })}
             </section>
         </>
